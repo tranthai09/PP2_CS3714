0aad45fe4b76f0302900bf7f606f53455b58d9bd
tranthai <tranthai@vt.edu>
2021-03-18 11:37:09 -0400

    Create 2 classes

---
+++ app/src/main/java/com/example/pp2_cs3714/MusicCompletionRecevier.java
@@ -0,0 +1,24 @@
+package com.example.pp2_cs3714;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+public class MusicCompletionRecevier extends BroadcastReceiver {
+
+    MainActivity mainActivity;
+
+    public MusicCompletionReceiver(){
+        //empty constructor
+    }
+
+    public MusicCompletionReceiver(MainActivity mainActivity) {
+        this.mainActivity= mainActivity;
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        String musicName= intent.getStringExtra(MusicService.MUSICNAME);
+        mainActivity.updateName(musicName);
+    }
+}
